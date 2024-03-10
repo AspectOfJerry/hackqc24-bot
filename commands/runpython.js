f558b5ab8f2d8475c4d2ea5740d6c521f171950c
Jerry <jerryfu_mc@outlook.com>
2024-03-10 18:15:37 -0400

    added python runner, needs testing

---
+++ commands/runpython.js
@@ -0,0 +1,18 @@
+import {exec} from "child_process";
+import {SlashCommandBuilder} from "@discordjs/builders";
+
+export default {
+    data: new SlashCommandBuilder()
+    .setName("runpython")
+    .setDescription("Run python."),
+    async execute(client, interaction) {
+        exec("python ../scripts/hello.py", (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+            console.error(`stderr: ${stderr}`);
+        });
+    }
+};
